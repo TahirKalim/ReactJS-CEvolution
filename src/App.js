2be5996c974b62963f048e6f5bb326d8631e48d5
Tahir Kalim <thrkalim@gmail.com>
2020-07-29 16:41:53 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+import Greet from "./components/Greet";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      {/*
+      <Message />
+      <Greet name="Tahir" heroName="Batman" />
+      
+      <Greet name="Tahir" heroName="Batman" />
+      <p>This is children class</p>
+      <Greet name="Ather" heroName="Ironman" />
+      <button>Action</button>
+  <Greet name="Danish" heroName="superman" />*/}
+
+      <h1>Hello CodeSandbox</h1>
+    </div>
+  );
+}
+
+export default App;
